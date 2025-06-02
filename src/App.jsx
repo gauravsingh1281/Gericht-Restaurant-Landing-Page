61eab80e44ac0d529e8a2497f2f3281354ad7ec5
Gaurav Pratap Singh <gs492979@gmail.com>
2025-06-03 02:48:58 +0530

    About us section completed.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Navbar from "./components/Navbar/Navbar";
 import "./App.css";
 import Header from "./container/Header/Header";
+import AboutUs from "./container/AboutUs/AboutUs";
 export default function App() {
   return (
     <>
       <Navbar />
       <Header />
+      <AboutUs />
     </>
   );
 }
