709e234b864b2c539be93bcfbc31cd1bed48eb9c
Gaurav Pratap Singh <gs492979@gmail.com>
2025-06-02 23:51:35 +0530

    basic initial data added.

---
+++ src/constants/images.js
@@ -0,0 +1,47 @@
+import bg from "../assets/images/bg.png";
+import chef from "../assets/images/chef.png";
+import G from "../assets/images/G.png";
+import gallery01 from "../assets/images/gallery01.png";
+import gallery02 from "../assets/images/gallery02.png";
+import gallery03 from "../assets/images/gallery03.png";
+import gallery04 from "../assets/images/gallery04.png";
+import knife from "../assets/images/knife.png";
+import logo from "../assets/images/logo.png";
+import menu from "../assets/images/menu.png";
+import overlaybg from "../assets/images/overlaybg.png";
+import spoon from "../assets/images/spoon.svg";
+import welcome from "../assets/images/welcome.png";
+import findus from "../assets/images/findus.png";
+import laurels from "../assets/images/laurels.png";
+import award01 from "../assets/images/award01.png";
+import award02 from "../assets/images/award02.png";
+import award03 from "../assets/images/award03.png";
+import award05 from "../assets/images/award05.png";
+import sign from "../assets/images/sign.png";
+import quote from "../assets/images/quote.png";
+import gericht from "../assets/images/gericht.png";
+
+export default {
+  bg,
+  chef,
+  G,
+  gallery01,
+  gallery02,
+  gallery03,
+  gallery04,
+  knife,
+  logo,
+  menu,
+  overlaybg,
+  spoon,
+  welcome,
+  findus,
+  laurels,
+  award01,
+  award02,
+  award03,
+  award05,
+  sign,
+  quote,
+  gericht,
+};
