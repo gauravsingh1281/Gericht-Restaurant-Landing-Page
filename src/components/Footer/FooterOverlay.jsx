5108e20e407330c95ac5c2a509e0e3124f2b0b16
Gaurav Pratap Singh <gs492979@gmail.com>
2025-06-04 02:36:32 +0530

    removed unused files

--- src/components/Footer/FooterOverlay.jsx
+++
@@ -1,9 +0,0 @@
-import "./FooterOverlay.css";
-export default function FooterOverlay() {
-  return (
-    <div className="app__footerOverlay">
-      <div className="app__footerOverlay-black"></div>
-      <div className="app__footerOverlay-img app__bg"></div>
-    </div>
-  );
-}
