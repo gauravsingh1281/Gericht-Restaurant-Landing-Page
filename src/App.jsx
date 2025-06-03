1913c32e9c6ae184f7c329802132e9e963f148c0
Gaurav Pratap Singh <gs492979@gmail.com>
2025-06-03 16:26:23 +0530

    menu section completed.

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import Navbar from "./components/Navbar/Navbar";
 import "./App.css";
 import Header from "./container/Header/Header";
 import AboutUs from "./container/AboutUs/AboutUs";
+import SpecialMenu from "./container/Menu/SpecialMenu";
 export default function App() {
   return (
     <>
       <Navbar />
       <Header />
       <AboutUs />
+      <SpecialMenu />
     </>
   );
 }
